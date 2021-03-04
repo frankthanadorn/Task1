fd68764e36d84549ac5bceaaa3beae6a17b3e8f7
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-03-04 18:32:38 +0700

    add button and style

--- kslider/src/App.js
+++ kslider/src/App.js
@@ -1,25 +1,35 @@
 import Slider from "./Slider";
 import Kodava from "./Circle";
-
 import './App.css';
 import React,{useState} from 'react';
+import styled from "styled-components";
 
-function generateShapes() {
-  return [...Array(50)].map((_, i) => ({
-    id: i.toString(),
-    x: Math.random() * 360,
-    y: Math.random() * 438,
-    weight: (Math.random()*10).toFixed(2),
-    
-  }));
-}
-const INITIAL_STATE = generateShapes();
+const Button = styled.button`
+background-color: #3f51b5;
+color: white;
+font-size: 20px;
+padding: 10px 60px;
+border-radius: 5px;
+margin: 10px 0px;
+cursor: pointer;
+ 
+`;
+
 
 function App() {
   const [value, setValue] = useState([0.00, 10.00]);
 
-  const [circles, setCircles] = useState(INITIAL_STATE);
-
+  const [circles, setCircles] = useState([]);
+ function generateShapes() {
+    return setCircles([...Array(50)].map((_, i) => ({
+      id: i.toString(),
+      x: Math.random() * 360,
+      y: Math.random() * 438,
+      weight: (Math.random()*10).toFixed(2),
+  
+    })));
+  }
+  
   const data = circles.filter((circle) => {
   return ((parseFloat(circle.weight) >= value[0] 
   && parseFloat(circle.weight) <= value[1])) 
@@ -34,7 +44,9 @@ function App() {
         
         <Kodava circles={data} setCircles={setCircles}/>
         <br/>
+        <Button onClick={generateShapes}>Reset</Button>
         <Slider value={value} setValue={setValue}/>
+        
       </header>
     </div>
   );
